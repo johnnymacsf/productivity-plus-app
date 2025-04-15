48cc005296211494ac2e151d0722b9c2209bd369
John Gill <johnnymacsf@outlook.com>
2025-04-15 15:06:42 -0700

    Provided backendurl from host

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,8 @@ export const AppContext = createContext();
 export const AppContextProvider = (props) => {
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    //const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl ='https://productivity-plus-backend.onrender.com'
     const [isLoggedin, setIsLoggedin] = useState(false);
     const [userData, setUserData] = useState(false);
 
