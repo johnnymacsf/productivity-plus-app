88da16032853ca77b9014b4dfe524bd93532888a
John Gill <johnnymacsf@outlook.com>
2025-04-15 15:13:01 -0700

    Updated CORS security policy to include website link for allowed origins

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://productivity-plus-frontend.onrender.com/'];
 
 app.use(express.json());
 app.use(cookieParser());
